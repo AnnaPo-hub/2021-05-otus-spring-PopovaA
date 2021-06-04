ea5aaecf84b41cfc6c4946003ffcdae5dd70acd9
Anna Popova <anna.popova201909@gmail.com>
2021-06-04 15:22:12 +0300

    добавила классы приложения

---
+++ src/test/java/ru/otus/spring/service/QuestionnaireServiceTest.java
@@ -0,0 +1,27 @@
+package ru.otus.spring.service;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import ru.otus.spring.dao.QuestionDaoSimple;
+
+import java.io.FileNotFoundException;
+
+import static org.mockito.Mockito.*;
+
+class QuestionnaireServiceTest {
+
+    private QuestionnaireService service;
+    private QuestionDaoSimple dao;
+
+    @Before
+    public void setUp(){
+        dao = mock(QuestionDaoSimple.class);
+        service = new QuestionnaireService(dao);
+    }
+
+    @Test
+    public void shouldTestShowQuestionWasCalled () throws FileNotFoundException {
+       service.playQuestions();
+       verify(dao,times(1)).showQuestion();
+    }
+}
