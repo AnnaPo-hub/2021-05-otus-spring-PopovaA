b4bceadd65062fc0e46215014f62cba6b8274e8b
Anna Popova <anna.popova201909@gmail.com>
2021-06-09 23:35:24 +0300

    добавила тесты

--- src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
+++ src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
@@ -18,13 +18,13 @@ class QuestionDaoSimpleTest {
     }
 
     @Test
-    void shouldShowRPositiveResults() throws FileNotFoundException {
+    void shouldShowPositiveResult() throws FileNotFoundException {
         assertTrue(dao.showResults(5));
     }
 
     @Test
     void shouldGetQuestionsFromFile() throws FileNotFoundException {
-        final List<Question> questionsFromFile = dao.getQuestionsFromFile("questionnaire.csv");
-        assertEquals(1, questionsFromFile.size());
+        final List<Question> questionsFromFile = dao.getQuestionsFromFile("target/classes/questionnaire.csv");
+        assertEquals(5, questionsFromFile.size());
     }
 }
--- src/test/java/ru/otus/spring/service/QuestionnaireServiceTest.java
+++ src/test/java/ru/otus/spring/service/QuestionnaireServiceTest.java
@@ -1,9 +1,7 @@
 package ru.otus.spring.service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.otus.spring.dao.QuestionDaoSimple;
 
 import java.io.FileNotFoundException;
@@ -17,26 +15,15 @@ class QuestionnaireServiceTest {
     private QuestionDaoSimple dao;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         dao = new QuestionDaoSimple();
         service = new QuestionnaireServiceImpl(dao);
     }
 
     @Test
-    public void shouldTestShowQuestionWasCalled () throws FileNotFoundException {
-       service.playQuestions();
-       verify(dao,times(1)).showQuestion();
-    }
-
-    @Test
-    public void shouldCheck() throws FileNotFoundException {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext();
-        QuestionnaireService service = context.getBean(QuestionnaireService.class);
+    public void shouldTestShowQuestionWasCalled() throws FileNotFoundException {
         service.playQuestions();
-        Assert.assertNotNull(context);
-        Assert.assertNotNull(context.getBean("questionnaireService"));
-
-
+        verify(dao, times(1)).showQuestion();
     }
 }
+
