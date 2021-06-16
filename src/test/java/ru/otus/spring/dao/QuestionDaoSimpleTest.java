7b7a73cb833cc9ccde5f3a0d8ef72e862798e9de
Anna Popova <anna.popova201909@gmail.com>
2021-06-16 10:05:25 +0300

    added tests

--- src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
+++ src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
@@ -1,25 +1,31 @@
 package ru.otus.spring.dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.otus.spring.domain.Question;
 import ru.otus.spring.utils.QuestionnaireResults;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 class QuestionDaoSimpleTest {
     QuestionDaoSimple dao = new QuestionDaoSimple();
     QuestionnaireResults questionnaireResults = new QuestionnaireResults();
 
 
-//    @Test
-//    void shouldShowNegativeResult() throws FileNotFoundException {
-//        assertFalse(results.showResults(1));
-//    }
-//
-//    @Test
-//    void shouldShowPositiveResult() throws FileNotFoundException {
-//        assertTrue(dao.showResults(5));
-//    }
-//
-//    @Test
-//    void shouldGetQuestionsFromFile() throws FileNotFoundException {
-//        final List<Question> questionsFromFile = dao.getQuestionsFromFile("target/classes/questionnaire.csv");
-//        assertEquals(5, questionsFromFile.size());
-//    }
+    @Test
+    void shouldShowNegativeResult() throws FileNotFoundException {
+        Assertions.assertFalse(questionnaireResults.showResults("Ivan Ivanov", 1));
+    }
+
+    @Test
+    void shouldShowPositiveResult() throws FileNotFoundException {
+        Assertions.assertTrue(questionnaireResults.showResults("Ivan Ivanov", 5));
+    }
+
+    @Test
+    void shouldGetQuestionsFromFile() throws FileNotFoundException {
+        final List<Question> questionsFromFile = dao.getQuestionsFromFile("target/classes/questionnaire.csv");
+        Assertions.assertEquals(5, questionsFromFile.size());
+    }
 }
