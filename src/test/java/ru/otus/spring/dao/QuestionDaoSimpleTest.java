12c804c58d48e3b14bbedab6727f55659035d6a7
Anna Popova <anna.popova201909@gmail.com>
2021-06-17 15:11:10 +0300

    добавила тесты на проверку листа вопросов

--- src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
+++ src/test/java/ru/otus/spring/dao/QuestionDaoSimpleTest.java
@@ -8,6 +8,8 @@ import ru.otus.spring.utils.QuestionnaireResults;
 import java.io.FileNotFoundException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class QuestionDaoSimpleTest {
     QuestionDaoSimple dao = new QuestionDaoSimple();
     QuestionnaireResults questionnaireResults = new QuestionnaireResults();
@@ -20,7 +22,7 @@ class QuestionDaoSimpleTest {
 
     @Test
     void shouldShowPositiveResult() {
-        Assertions.assertTrue(questionnaireResults.checkResults(3));
+        assertTrue(questionnaireResults.checkResults(3));
     }
 
     @Test
@@ -38,6 +40,20 @@ class QuestionDaoSimpleTest {
     @Test
     void shouldGetQuestionsFromFile() throws FileNotFoundException {
         final List<Question> questionsFromFile = dao.getQuestionsFromFile("target/classes/questionnaire.csv");
+        for (Question question : questionsFromFile) {
+            System.out.println(question);
+        }
         Assertions.assertEquals(5, questionsFromFile.size());
+    }
+
+    @Test
+    void shouldCheckQuestionsListContentFromFile() throws FileNotFoundException {
+        final List<Question> questionsFromFile = dao.getQuestionsFromFile("target/classes/questionnaire.csv");
+        Question question1 = new Question("1", "Who was the heir to the throne in Minas Tirith?", "Aragorn");
+        Question question2 = new Question("2", "How many gnomes went with the hobbit Bilbo for the treasure?", "13");
+        Question question4 = new Question("4", "Who forged the ring of omnipotence?", "Sauron");
+        assertTrue(questionsFromFile.contains(question1));
+        assertTrue(questionsFromFile.contains(question2));
+        assertTrue(questionsFromFile.contains(question4));
     }
 }
